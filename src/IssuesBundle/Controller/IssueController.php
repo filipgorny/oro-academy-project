cf2b3af0b169d5a47bf9e8fe5a0bb4451b366000
Filip Górny <filip.gorny@gmail.com>
2016-05-11 17:26:36 +0200

    issue view page draft

--- src/IssuesBundle/Controller/IssueController.php
+++ src/IssuesBundle/Controller/IssueController.php
@@ -5,6 +5,9 @@ namespace IssuesBundle\Controller;
 use IssuesBundle\Entity\Issue;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
+use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
 
 /**
  * @Route("/issue")
@@ -12,11 +15,38 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class IssueController extends Controller
 {
     /**
-     * @Route("/view/{id}", name="orocrm_issue_view", requirements={"id"="\d+"})
+     * @Route("/", name="issues.issues_index", requirements={"id"="\d+"})
+     */
+    public function indexAction(Issue $issue)
+    {
+        return new Response();
+    }
+
+    /**
+     * @Route("/view/{id}", name="issues.issue_view", requirements={"id"="\d+"})
+     * @AclAncestor("issues.issue_view")
      * @Template
      */
     public function viewAction(Issue $issue)
     {
-        return array('entity' => $issue);
+        return array(
+            'entity' => $issue,
+        );
+    }
+
+    /**
+     * @Route("/update/{id}", name="issues.issue_update", requirements={"id"="\d+"})
+     */
+    public function updateAction(Issue $issue)
+    {
+        return new Response();
+    }
+
+    /**
+     * @Route("/update/{id}", name="issues.issue_api_delete", requirements={"id"="\d+"})
+     */
+    public function deleteAction(Issue $issue)
+    {
+        return new Response();
     }
 }
